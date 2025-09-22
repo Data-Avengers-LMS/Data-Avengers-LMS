910261bf623c5c5655505ab17e16b4cb459c7bf7
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2025-09-22 18:44:41 +0530

    fix(web): remove unused ThemeImage component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,29 +1,9 @@
-import Image, { type ImageProps } from "next/image";
-
-
-
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
-
 export default function Home() {
   return (
-   <div>
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      This is from data avengers
-    </main>
-   </div>
+    <div>
+      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+        This is from data avengers
+      </main>
+    </div>
   );
 }
