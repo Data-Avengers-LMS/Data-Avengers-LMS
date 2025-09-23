448f4d2746b31eaa8a1c5b7b0cf57133543f8bde
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2025-09-24 02:27:43 +0530

    feat: added shadcn for designing

--- backend/express-server/eslint.config.mjs
+++ backend/express-server/eslint.config.mjs
@@ -1,4 +1,11 @@
-import { config } from "@repo/eslint-config/base";
+import { config } from '@repo/eslint-config/base';
 
 /** @type {import("eslint").Linter.Config} */
-export default config;
+export default [
+  ...config,
+  {
+    rules: {
+      'import/no-unresolved': 'off'
+    }
+  }
+];
