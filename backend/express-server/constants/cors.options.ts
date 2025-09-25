59cb82983b7b8c9eaf07bcc73ac459834c8c81b5
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2025-09-25 11:42:23 +0530

    refactor: rebase testing branch

---
+++ backend/express-server/constants/cors.options.ts
@@ -0,0 +1,18 @@
+import { env } from "@environments/environment";
+
+export const corsOptions = {
+  origin: (
+    origin : string| undefined,
+    callback: (err: Error | null, status: boolean) => void
+  ) => {
+    if (origin === env.CLIENT_ORIGIN || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"), false);
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
