8d4589d48b2089bbcf55284d965dbfa47c72f6fc
XVIKASINGh <vikasksingh1705@gmail.com>
2025-10-07 17:11:03 +0530

    feat(queue): fix the createQueue issue

--- packages/common/queues/createqueues.ts
+++ packages/common/queues/createqueues.ts
@@ -1,8 +1,8 @@
 import { Queue, QueueOptions } from 'bullmq';
 import { env } from '@env/environments.z.js';
 
-export const createQueue = (name: string, options?: Partial<QueueOptions>) => {
-  return new Queue(name, {
+export const createQueue = (name: string, options?: Partial<QueueOptions>) =>
+  new Queue(name, {
     connection: {
       host: env.REDIS_HOST || 'localhost',
       port: Number(env.REDIS_PORT) || 6379,
@@ -15,4 +15,3 @@ export const createQueue = (name: string, options?: Partial<QueueOptions>) => {
     },
     ...options,
   });
-};
