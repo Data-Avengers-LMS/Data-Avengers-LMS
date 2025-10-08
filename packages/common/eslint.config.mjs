1737d8abafec2832d5ef2c5f4d1ace48a2715976
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2025-10-08 22:38:49 +0530

    fix: fixed package.json file

--- packages/common/eslint.config.mjs
+++ packages/common/eslint.config.mjs
@@ -1,12 +1,23 @@
-import { config } from '@repo/eslint-config/base.js';
+import { config } from '@repo/eslint-config/base';
 
 export default [
   ...config,
+  {
+    rules: {
+      'import/no-extraneous-dependencies': 'off',
+    },
+  },
   {
     files: ['**/*.ts'],
     rules: {
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+  {
+    files: ['**/examples/**/*.ts'],
+    rules: {
+      'no-console': 'off',
     },
   },
 ];
